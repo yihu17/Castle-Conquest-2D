132efd280617c091f0a3e6be070c34931d9a5a04
DESKTOP-0U1DOOA\Chester <chester.huang99@gmail.com>
2021-09-22 23:08:08 +0700

    Player now has running animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
     protected Rigidbody2D thisRigidBody;
+    protected Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         thisRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,13 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlFlow * runSpeed, thisRigidBody.velocity.y);
         thisRigidBody.velocity = playerVelocity;
         FlipSprite();
+        ChangingToRunningState();
+    }
+
+    private void ChangingToRunningState()
+    {
+        bool horizontalRun = Mathf.Abs(thisRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", horizontalRun);
     }
 
     private void FlipSprite()
