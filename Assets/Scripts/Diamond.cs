1d3fb00d6f052d4d8eef39a46ac704ec5eba4ff8
DESKTOP-0U1DOOA\Chester <chester.huang99@gmail.com>
2021-09-30 14:33:58 +0700

    Added pick up audio to hearts and diamonds

--- Assets/Scripts/Diamond.cs
+++ Assets/Scripts/Diamond.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Diamond : MonoBehaviour
 {
+    [SerializeField] AudioClip diamondSFX;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(diamondSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Hearts.cs
+++ Assets/Scripts/Hearts.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Hearts : MonoBehaviour
 {
+    [SerializeField] AudioClip heartSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(heartSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
